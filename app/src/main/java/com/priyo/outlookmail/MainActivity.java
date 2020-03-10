12996dc6f3dd00de1efa68d946cd879211cbae0d
Priyobroto <Priobroto99@gmail.com>
2020-03-10 11:20:50 +0530

    initial commit

---
+++ app/src/main/java/com/priyo/outlookmail/MainActivity.java
@@ -0,0 +1,59 @@
+package com.priyo.outlookmail;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading ...");
+        dialog.show();
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                dialog.dismiss();
+                //show webview
+                webView.setVisibility(View.VISIBLE);
+            }
+        });
+        webView.loadUrl("https://outlook.office.com/");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private static final int TIME_INTERVAL = 2000; // # milliseconds, desired time passed between two back presses.
+    private long mBackPressed;
+
+    @Override
+    public void onBackPressed()
+    {
+        if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis())
+        {
+            super.onBackPressed();
+            return;
+        }
+        else { Toast.makeText(getBaseContext(), "Tap back button to exit", Toast.LENGTH_SHORT).show(); }
+
+        mBackPressed = System.currentTimeMillis();
+    }
+}
